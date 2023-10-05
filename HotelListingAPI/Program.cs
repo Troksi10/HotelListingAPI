e3c084a3c4d30588742d96b0768c2e4c15059737
lLija <ilija.trokovski@gmail.com>
2023-10-06 01:34:19 +0200

    Serilog & Seq

--- HotelListingAPI/Program.cs
+++ HotelListingAPI/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        b => b
+    .AllowAnyHeader()
+    .AllowAnyOrigin()
+    .AllowAnyMethod());
+});
+
+builder.Host.UseSerilog((ctx,lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,7 +28,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSerilogRequestLogging();
+
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
