bcaf2b4cef2f6a126b60cc54d1de4bb82f91b6bb
lLija <ilija.trokovski@gmail.com>
2023-10-06 02:06:39 +0200

    Setup Entity Framework

---
+++ HotelListingAPI/Data/HotelListingDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListingAPI.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+    }
+}
