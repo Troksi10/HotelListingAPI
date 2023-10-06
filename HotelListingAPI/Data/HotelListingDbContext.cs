eaf62d94a5bd5bf2a0d924629474920d4c45034a
lLija <ilija.trokovski@gmail.com>
2023-10-06 15:29:41 +0200

    Adding Countries EndPoint

--- HotelListingAPI/Data/HotelListingDbContext.cs
+++ HotelListingAPI/Data/HotelListingDbContext.cs
@@ -8,5 +8,43 @@ namespace HotelListingAPI.Data
         {
             
         }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country() { Id = 1, Name = "Macedonia", ShortName = "MK" },
+                new Country() { Id = 2, Name = "Serbia", ShortName = "SRB" },
+                new Country() { Id = 3, Name = "Croatia", ShortName = "HR" }
+                );
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel() 
+                {   Id = 1,
+                    Name = "Aurora Resort and Spa",
+                    Address = "Ul.3 bb",
+                    CountryId = 1,
+                    Rating = 4.4
+                },
+                new Hotel()
+                {
+                    Id = 2,
+                    Name = "Viceroy Kopaonik Serbia",
+                    Address = "Josifa Pančića 12, 36354 Kopaonik",
+                    CountryId = 2,
+                    Rating = 4.8
+                },
+                new Hotel()
+                {
+                    Id = 3,
+                    Name = "Hotel Ora",
+                    Address = "Poljička cesta 26E, 21000 Split",
+                    CountryId = 3,
+                    Rating = 3.7
+                }
+                );
+        }
     }
 }
---
+++ HotelListingAPI/Migrations/20231006105241_SeededCountriesAndHotels.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeededCountriesAndHotels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Hotels",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Hotels",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "Macedonia", "MK" },
+                    { 2, "Serbia", "SRB" },
+                    { 3, "Croatia", "HR" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Ul.3 bb", 1, "Aurora Resort and Spa", 4.4000000000000004 },
+                    { 2, "Josifa Pančića 12, 36354 Kopaonik", 2, "Viceroy Kopaonik Serbia", 4.7999999999999998 },
+                    { 3, "Poljička cesta 26E, 21000 Split", 3, "Hotel Ora", 3.7000000000000002 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Hotels",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Hotels",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
